295d98bc968508fe9e94ffdcd79792ce19614917
smwbtech <smwb.tech@gmail.com>
2019-12-27 11:55:05 +0300

    Update App.svelte
    * Changed all customization layers created in in each cycle

--- src/App.svelte
+++ src/App.svelte
@@ -38,12 +38,6 @@
 		}
 	};
 
-	$: walls = `img/walls/walls_${customization.walls.color}.png`;
-	$: roof = `img/roof/roof_${customization.roof.color}.png`;
-	$: windows = `img/windows/windows_${customization.windows.color}.png`;
-	$: plumbs = `img/plumbs/plumbs_${customization.plumbs.color}.png`;
-	$: drains = `img/drains/drains_${customization.drains.color}.png`;
-
 	function changeItemHandler(e) {
 		const { name, color } = e.detail;
 		customization[name].color = color;
@@ -54,30 +48,16 @@
 
 <div class="house-customizer-wrapper">
 
-	<!-- Drains customization -->
-	{#if customization.drains.color !== 'default'}
-		<img class="customization" src="{drains}" alt="выбор цвета сливов" />
-	{/if}
-
-	<!-- Plumbs customization -->
-	{#if customization.plumbs.color !== 'default'}
-		<img class="customization" src="{plumbs}" alt="выбор цвета окон" />
-	{/if}
-
-	<!-- Windows customization -->
-	{#if customization.windows.color !== 'default'}
-		<img class="customization" src="{windows}" alt="выбор цвета окон" />
-	{/if}
-
-	<!-- Walls customization -->
-	{#if customization.walls.color !== 'default'}
-		<img class="customization" src="{walls}" alt="выбор цвета стен" />
-	{/if}
-
-	<!-- Roof customization -->
-	{#if customization.roof.color !== 'default'}
-		<img class="customization" src="{roof}" alt="выбор цвета крыши" />
-	{/if}
+	<!-- Customization layers -->
+	{#each Object.keys(customization) as key}
+		{#if customization[key].color !== 'default'}
+			<img
+				class="customization"
+				src="{`img/${key}/${key}_${customization[key].color}.png`}"
+				alt="кастомизация элемента дома"
+			/>
+		{/if}
+	{/each}
 
 	<!-- House background image -->
 	<img src="img/house.jpg" alt="Изображение стандартного дома" />
