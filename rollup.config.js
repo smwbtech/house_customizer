7fe719cd1fa9a56d788695af6cafcc13ccde0e4a
smwbtech <smwb.tech@gmail.com>
2020-01-22 20:29:23 +0300

    Added Babel

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import autoPreprocess from 'svelte-preprocess';
 import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -26,7 +27,20 @@ export default {
 				css.write('public/build/bundle.css');
 			}
 		}),
-
+		babel({
+			babelrc: false,
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							browsers: ['last 2 version', '> 2%', 'IE 11']
+						},
+						modules: false
+					}
+				]
+			]
+		}),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration —
